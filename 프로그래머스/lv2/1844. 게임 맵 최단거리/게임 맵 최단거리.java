0574480a3d7199fabe468d1847a6ad377962874d
LeeGaeun <93987703+nueag@users.noreply.github.com>
2023-10-06 15:32:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 2.71 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,43 +4,50 @@ import java.util.Queue;
 
 class Solution {
     
-   public static int solution(int[][] maps) {
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int rows = maps.length;
-        int columns = maps[0].length;
-
-        boolean[][] visited = new boolean[rows][columns];
+   public static int solution(int[][] maps){
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{0, 0});
-        visited[0][0] = true;
-        int count = 1; // 시작 위치도 카운트에 포함
+        boolean isFinished = false;
+        int count =0;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] current = queue.poll();
-                int x = current[0];
-                int y = current[1];
-                
-                if (x == rows - 1 && y == columns - 1) {
-                    return count; // 목적지에 도달했을 때 탐색 종료
+        while(!queue.isEmpty()) {
+            int tmpSize = queue.size();
+            for(int i = 0; i < tmpSize; i++) {
+                int[] tmp = queue.poll();
+                bfs(queue, visited, maps, tmp[0], tmp[1]);
+                if(tmp[0] == maps.length-1 && tmp[1] == maps[0].length-1) {
+                    count++;
+                    return count;
                 }
 
-                for (int[] dir : directions) {
-                    int newX = x + dir[0];
-                    int newY = y + dir[1];
-                    if (isValid(newX, newY, rows, columns) && maps[newX][newY] == 1 && !visited[newX][newY]) {
-                        queue.add(new int[]{newX, newY});
-                        visited[newX][newY] = true;
-                    }
-                }
             }
+            
             count++;
         }
+        return -1;
+    }
 
-        return -1; // 목적지에 도달하지 못한 경우
+
+    public static void bfs(Queue<int[]>queue, boolean[][] visited, int[][] matrix, int x, int y) {
+
+        if(x + 1 < matrix.length && matrix[x + 1][y] == 1 && !visited[x + 1][y]) {
+            queue.add(new int[]{x+1, y});
+            visited[x+1][y] = true;
+        }
+        if(x - 1 >= 0 && matrix[x - 1][y] == 1 && !visited[x - 1][y]) {
+            queue.add(new int[]{x-1, y} );
+            visited[x - 1][y] = true;
+        }
+        if(y + 1 < matrix[0].length && matrix[x][y+1] == 1 && !visited[x][y+1]) {
+            queue.add(new int[]{x, y+1});
+            visited[x][y+1] = true;
+        }
+        if(y - 1 >= 0 && matrix[x][y-1] == 1 && !visited[x][y-1]) {
+            queue.add(new int[]{x, y-1});
+            visited[x][y-1] = true;
+        }
+        visited[x][y] = true;
     }
-    private static boolean isValid(int x, int y, int rows, int columns) {
-        return x >= 0 && x < rows && y >= 0 && y < columns;
-    }
+
 }
