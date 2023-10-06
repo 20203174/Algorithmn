a6a2303cb064dc9d7623ce3f79b89d2d94ad03ee
LeeGaeun <93987703+nueag@users.noreply.github.com>
2023-10-06 15:16:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.00 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,36 +5,42 @@ import java.util.Queue;
 class Solution {
     
    public static int solution(int[][] maps) {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        int[][] mapArr = maps;
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        int count = 0;
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int rows = maps.length;
+        int columns = maps[0].length;
 
-        queue.add(new int[] {0, 0});
+        boolean[][] visited = new boolean[rows][columns];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
         visited[0][0] = true;
+        int count = 1; // 시작 위치도 카운트에 포함
+
         while (!queue.isEmpty()) {
             int size = queue.size();
-                int[] tmp = queue.poll();
-                for (int i = 0; i < 4; i++) {
-                    if (tmp[0] + dx[i] >= 0 && tmp[0] + dx[i] < mapArr.length && tmp[1] + dy[i] >= 0 &&
-                            tmp[1] + dy[i] < mapArr[0].length) {
-                        if (mapArr[tmp[0] + dx[i]][tmp[1] + dy[i]] == 1 && !visited[tmp[0] + dx[i]][tmp[1] + dy[i]]) {
-                            queue.add(new int[] {tmp[0] + dx[i], tmp[1] + dy[i]});
-                            mapArr[tmp[0] + dx[i]][tmp[1] + dy[i]] += mapArr[tmp[0]][tmp[1]];
-                            visited[tmp[0] + dx[i]][tmp[1] + dy[i]] = true;
-                        }
-                    }
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+                int x = current[0];
+                int y = current[1];
                 
-            }
+                if (x == rows - 1 && y == columns - 1) {
+                    return count; // 목적지에 도달했을 때 탐색 종료
+                }
 
+                for (int[] dir : directions) {
+                    int newX = x + dir[0];
+                    int newY = y + dir[1];
+                    if (isValid(newX, newY, rows, columns) && maps[newX][newY] == 1 && !visited[newX][newY]) {
+                        queue.add(new int[]{newX, newY});
+                        visited[newX][newY] = true;
+                    }
+                }
+            }
+            count++;
         }
-        count = mapArr[maps.length-1][maps[0].length-1];
-        if (count == 1) {
-            count = -1;
-        }
-//        System.out.println(count);
-        return count;
+
+        return -1; // 목적지에 도달하지 못한 경우
+    }
+    private static boolean isValid(int x, int y, int rows, int columns) {
+        return x >= 0 && x < rows && y >= 0 && y < columns;
     }
 }
