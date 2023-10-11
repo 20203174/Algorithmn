0796608ff770a6b127c215826b9332962e09ce09
20203174 <kaeun0000@naver.com>
2023-10-11 13:48:49 +0900

    1011 Algorithmn

---
+++ Algorithmn_java/Algorithmn_maven/src/main/java/org/example/_1011/Main.java
@@ -0,0 +1,56 @@
+package org.example._1011;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static Node root = null;
+    public static class Node{
+        Node left = null;
+        Node right = null;
+        int key;
+    }
+    public static void add(int key) {
+        Node newNode = new Node();
+        newNode.key = key;
+
+        if(root == null) {
+            root = newNode;
+        } else {
+            root = addNode(root, newNode);
+        }
+    }
+    public static Node addNode(Node node, Node newNode) {
+        if(node == null) {
+            return newNode;
+        } else if(node.key > newNode.key) {
+            node.left = addNode(node.left, newNode);
+        } else if(node.key < newNode.key){
+            node.right = addNode(node.right, newNode);
+        }
+        return node;
+    }
+    public static void printNode(Node node) {
+        if(node.left != null) {
+            printNode(node.left);
+        }
+        if(node.right != null) {
+            printNode(node.right);
+        }
+        System.out.println(node.key);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            String line = br.readLine();
+            if(line == null || line.equals("")) {
+                break;
+            }
+            add(Integer.parseInt(line));
+        }
+        printNode(root);
+    }
+}
