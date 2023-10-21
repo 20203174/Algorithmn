ddcfb2367f8bfce72ddcdf11e7a51995b2f522f3
LeeGaeun <93987703+nueag@users.noreply.github.com>
2023-10-22 00:31:58 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,47 +1,57 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[][] dp = new int[30][30];
- 
-		// 2번 성질 (n == r, r == 0)
-		for (int i = 0; i < 30; i++) {
-			dp[i][i] = 1;
-			dp[i][0] = 1;
-		}
-			
- 
-		for (int i = 2; i < 30; i++) {
-			for (int j = 1; j < 30; j++) {
-				// 1번 성질 
-				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-			}
-		}
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-        
-		for(int i = 0; i < T; i++) {
-			
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			// M개중 N개를 뽑는 경우이므로 nCr 에서 n = M, r = N이다.
-			int N = Integer.parseInt(st.nextToken());	// N = r
-			int M = Integer.parseInt(st.nextToken());	// M = n
-					
-			sb.append(dp[M][N]).append('\n');
-		}
-		
-		System.out.println(sb);
- 
-	}
+    static int[][] bridge = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] left = new int[N + 1];
+        int[] right = new int[N + 1];
+
+        for (int i = 0; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                if (i == j) {
+                    bridge[i][j] = 1;
+                }
+                if (i == 1) {
+                    bridge[i][j] = j;
+                }
+                bridge[0][j] = 1;
+            }
+        }
+        for (int i = 1; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                if (i == j || i == 1 || j == 1) {
+                    continue;
+                }
+                if (i > j) {
+                    continue;
+                }
+                int tmpi = i;
+                int tmpj = j;
+                while (tmpj >= tmpi) {
+                    bridge[i][j] += bridge[tmpi - 1][tmpj - 1];
+                    tmpj--;
+                }
+            }
+        }
+
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            left[i] = Integer.parseInt(st.nextToken());
+            right[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(bridge[left[i]][right[i]]).append('\n');
+        }
+        System.out.println(sb);
+    }
 }
