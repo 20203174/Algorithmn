9b0781ab48c5c6a3a8627fec5f709a2406c36ca5
LeeGaeun <93987703+nueag@users.noreply.github.com>
2024-05-15 11:01:02 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 195 ms, Memory: 26,608 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int j = 1; j <= 10; j++) {
+            N = Integer.parseInt(br.readLine());
+            int count = 0;
+            arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+
+            for(int i = 2; i < N-2; i++) {
+                count += Math.max(0, arr[i] - Math.max(Math.max(arr[i-2], arr[i-1]), Math.max(arr[i+2], arr[i+1])));
+            }
+
+            System.out.print("#" + j+ " " + count);
+            System.out.println();
+        }
+    }
+}
