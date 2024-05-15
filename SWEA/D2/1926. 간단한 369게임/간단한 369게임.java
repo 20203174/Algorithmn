17d305813688da330196825af37b1b0f39040995
LeeGaeun <93987703+nueag@users.noreply.github.com>
2024-05-15 10:30:51 +0900

    [D2] Title: 간단한 369게임, Time: 114 ms, Memory: 25,648 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= N; i++) {
+            String tmp = String.valueOf(i);
+            if(tmp.contains("3") || tmp.contains("6") || tmp.contains("9")) {
+                char[] arr = tmp.toCharArray();
+                for(int j = 0; j < arr.length; j++) {
+                    if(arr[j] == '3' || arr[j] == '6' || arr[j] == '9') {
+                        System.out.print("-");
+                    }
+                }
+            } else {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+        }
+
+    }
+}
